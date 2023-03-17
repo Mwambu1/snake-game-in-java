2b2c484463a7837a827da1546184ed39b232d0e0
Mwambu Kaumba <71637268+Mwambu1@users.noreply.github.com>
2023-03-17 12:18:30 +0200

    Create GameFrame.java

---
+++ GameFrame.java
@@ -0,0 +1,11 @@
+/* AUTHER: MWAMBU KAUMBA
+   DESCRIPTION: This file includes the main class for the program and in it initalize a game frame that contains the gamepanel 
+   with all the code for the rest of the game. */
+
+public class SnakeGame 
+{
+    public static void main(String[] args) throws Exception 
+    {
+         new GameFrame();
+    }
+}
